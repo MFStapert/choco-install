c5f1549ae0a505418e5184bc83ae8ad9a93d34f6
mfstapert <mf.stapert@gmail.com>
2021-12-14 21:02:43 +0100

    initial commit

---
+++ install.bat
@@ -0,0 +1,36 @@
+choco feature enable -n allowGlobalConfirmation
+
+# utils
+choco install 7zip
+choco install adobereader
+choco install dropbox
+choco install firefox
+choco install teamviewer
+
+# drivers
+choco install razer-synapse
+choco install geforce-experience
+
+# creative
+choco install audacity
+choco install kdenlive
+choco install obs-studio
+choco install paint.net
+
+# dev
+choco install postman
+choco install godot
+choco install git
+choco install nodejs
+choco install vscode
+
+# game
+choco install leagueoflegends
+choco install steam-client
+choco install VisualBoyAdvance
+choco install discord
+
+# media
+choco install spotify
+choco install vlc
+choco install utorrent	
