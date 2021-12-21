4a23e19f8cbab47bb4e648c4fd1d235ab16c8f95
mfstapert <mf.stapert@gmail.com>
2021-12-21 21:14:35 +0100

    fix comments, add obsidian

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 choco feature enable -n allowGlobalConfirmation
 
-# utils
+:: utils
 choco install 7zip
 choco install adobereader
 choco install bitwarden
@@ -8,30 +8,30 @@ choco install dropbox
 choco install firefox
 choco install teamviewer
 
-# drivers
+:: drivers
 choco install razer-synapse
 choco install geforce-experience
 
-# creative
+:: creative
 choco install audacity
 choco install kdenlive
 choco install obs-studio
 choco install paint.net
+choco install obsidian
 
-# dev
+:: dev
 choco install postman
 choco install godot
 choco install git
 choco install nodejs
 choco install vscode
 
-# game
+:: game
 choco install leagueoflegends
 choco install steam-client
-choco install VisualBoyAdvance
 choco install discord
 
-# media
+:: media
 choco install spotify
 choco install vlc
 choco install utorrent	
